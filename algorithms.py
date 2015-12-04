a806fff4ec937c6a6550ff0fb0d2e7d4049d4e31
Yuan Feng <yf2338@columbia.edu>
2015-12-04 13:35:52 -0500

    lsh main functions

--- algorithms.py
+++ algorithms.py
@@ -51,9 +51,70 @@ def vector_aggregation(m, alpha, k):
 def expand_and_aggregate():
 	pass
 
-
+###########################################Indyk LSH
 # TODO: LSH
+class LSHmain:
 	
+	def __init__(self, hashFamily, k, L):
+		super(LSH, self).__init__()
+		self.hashFamily = hashFamily
+		self.k = k
+		self.L = 0
+		self.hashTables = []
+		self.fillHashTables(L)
+	
+	def fillHashTables(self, L):
+		""" Fill or truncate hash tables """
+		if L < self.L:
+			self.hashTables = self.hashTables[:L]
+		else:
+			hashFunctions = []
+			for l in xrange(self.L, L):
+				# hash table l
+				func = []
+				for j in xrange(self.k):
+					# h_j
+					func.append(self.hashFamily.createHashFunction())
+				hashFunctions.append(func)
+			for h in hashFunctions:
+				self.hashTables.append((h, defaultdict(lambda:[])))
+	
+	def hash(self, h, p):
+		res = []
+		for func in h:
+			res.append(func.hash(p))
+		return self.hashFamily.combine(res)
+	
+	def index(self, dataset):
+		""" index points in dataset """
+		self.dataset = dataset
+		for h,table in self.hashTables:
+			for i_x,p in enumerate(self.dataset):
+				table[self.hash(h, p)].append(i_x)
+		self.totalTouched = 0
+		self.queryNum = 0
+	
+	def query(self, q, metric, resLimit):
+		""" according to the metric, find resLimit closest points to q """
+		uniqueClosePoints = set()
+		for h,table in self.hashTables:
+			closePoints = table.get(self.hash(h, q), [])
+			uniqueClosePoints.update(closePoints)
+		self.totalTouched += len(uniqueClosePoints)
+		self.queryNum += 1
+		tmp = []
+		for i_x in uniqueClosePoints:
+			tmp.append((i_x, metric(q, self.points[i_x])))
+		uniqueClosePoints = tmp
+		uniqueClosePoints.sort(key=itemgetter(1))
+		return uniqueClosePoints[:resLimit]
+
+	def getAverageTouched(self):
+		return self.totalTouched/self.queryNum
+
+
+
+
 # TODO: optimization multithreaded implementation and caches
 
 
